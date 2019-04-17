654a89c4ab8a4852cd93137911da95db4a401bf1
ZyrilErrol <zyrilerrol.gatchalian@student.supsi.ch>
2019-04-17 09:55:05 +0200

    implemented db (not functioning) and switch branch

---
+++ MeteoApp/Models/EntryToSave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteoApp.Models
+{
+    public class EntryToSave
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+
+    }
+}
